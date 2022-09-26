d7f22eb155d0340b8b1af2c54174cfc3a511014b
Felipe Steffan <felip.developer@hotmail.com>
2022-09-26 16:51:56 -0300

    Swagger Config

--- src/main/java/com/program/desafio/resources/UserResource.java
+++ src/main/java/com/program/desafio/resources/UserResource.java
@@ -1,12 +1,12 @@
 package com.program.desafio.resources;
 
 
-import java.lang.module.ResolutionException;
 import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,14 +14,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException.UnprocessableEntity;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.program.desafio.entities.User;
 import com.program.desafio.repositories.UserRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/users")
+@Api(value="API REST USERS")
+@CrossOrigin(origins="*")
 public class UserResource {
 	
 	@Autowired
@@ -29,12 +33,14 @@ public class UserResource {
 	
 	
 	@GetMapping
+	@ApiOperation(value= "estatísticas das transações que aconteceram" )
 	public ResponseEntity <List<User>> findAll() {
 		List<User> result = repository.findAll();
 		return ResponseEntity.ok(result);
 	}	
 	
 	@PostMapping
+	@ApiOperation(value= "transação consiste de um valor e uma dataHora de quando ela aconteceu")
 	public ResponseEntity<User> insert(@RequestBody User user) {	
 		user = repository.save(user);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
@@ -43,11 +49,11 @@ public class UserResource {
 	}	
 	
 	@DeleteMapping(value="/id")
+	@ApiOperation(value= "Apaga todos os dados de transação")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		repository.deleteAll();
 		return ResponseEntity.noContent().build();
 		
-	}
-	
+	}	
 	
 }
